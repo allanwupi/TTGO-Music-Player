3219da73c88c16029cae984de1032b445d8f5c02
Allan Wu <allan.wu@outlook.com.au>
2025-10-20 15:24:57 +0800

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,175 @@
+#include <Arduino.h>
+#include <TFT_eSPI.h>
+#include "songs.h"
+#include "pitches.h"
+
+#define TREBLE 1
+#define BASS 2
+#define TREBLE_BUZZER 1
+#define BASS_BUZZER 3
+
+#define LEFT_BUTTON 0
+#define RIGHT_BUTTON 14
+
+#define MENU_X_DATUM    10
+#define MENU_Y_DATUM    30
+#define BUFFER_CHARS    50
+
+#define LEFT_BUTTON     0
+#define RIGHT_BUTTON    14
+
+#define PRIMARY_TEXT_COLOUR     TFT_WHITE
+#define HIGH_EMPHASIS_COLOUR    TFT_GOLD
+#define LOW_EMPHASIS_COLOUR     0x2965
+#define BACKGROUND_COLOUR       TFT_BLACK
+
+TFT_eSPI TFT = TFT_eSPI();
+TFT_eSPI *tft;
+
+int screenOrientation = 3;
+int chosenSong = 0;
+
+void userSelectSong(TFT_eSPI *tft);
+void playSong(Song_t song, int barsToDisplay, TFT_eSPI *tft);
+
+void setup()
+{
+    pinMode(TREBLE_BUZZER, OUTPUT);
+    pinMode(BASS_BUZZER, OUTPUT);
+    pinMode(15, OUTPUT);
+    digitalWrite(15, HIGH);
+    tft = &TFT;
+    tft->init();
+    tft->setTextColor(PRIMARY_TEXT_COLOUR, BACKGROUND_COLOUR);
+    tft->setTextFont(0);
+    tft->setTextSize(2);
+    tft->setRotation(screenOrientation);
+    tft->fillScreen(BACKGROUND_COLOUR);
+	ledcSetup(TREBLE, 10000, 16);
+	ledcSetup(BASS, 10000, 16);
+	ledcAttachPin(TREBLE_BUZZER, TREBLE);
+	ledcAttachPin(BASS_BUZZER, BASS);
+    userSelectSong(tft);
+    //Serial.begin(115200);
+}
+
+void loop()
+{
+    switch (chosenSong) {
+        case (0):
+            playSong(Megalovania, 1, tft);
+            break;
+        case (1):
+            playSong(TheLegend0, 4, tft);
+            playSong(TheLegend1, 4, tft);
+            playSong(TheLegend2, 2, tft);
+            playSong(TheLegend3, 4, tft);
+            break;
+        default:
+            playSong(FreedomMotif, 2, tft);
+    }
+}
+
+void userSelectSong(TFT_eSPI *tft) {
+    int prevLeft = 0, prevRight = 0;
+    int currLeft = 0, currRight = 0;
+    int prevChoice = -1, currChoice = 0;
+    bool startPlayer = false;
+
+    tft->setCursor(0, 0);
+    tft->printf(" WORST MUSIC PLAYER EVER ");
+    tft->drawFastHLine(0, 20, 320, TFT_WHITE);
+
+    while (!startPlayer) {
+        currLeft = !digitalRead(LEFT_BUTTON);
+        currRight = !digitalRead(RIGHT_BUTTON);
+        if (prevLeft && !currLeft) {
+            if (currChoice == NUM_SONGS) {
+                screenOrientation = (screenOrientation > 1) ? 1 : 3;
+                tft->setRotation(screenOrientation);
+                tft->fillScreen(TFT_BLACK);
+                tft->setCursor(0, 0);
+                tft->printf(" WORST MUSIC PLAYER EVER ");
+                tft->drawFastHLine(0, 20, 320, TFT_WHITE);
+                prevChoice = -1;
+            }
+            else startPlayer = true;
+        } else if (prevRight && !currRight) {
+            currChoice = (currChoice + 1) % (NUM_SONGS+1);
+        }
+
+        if (prevChoice != currChoice) {
+            tft->setTextColor(LOW_EMPHASIS_COLOUR, BACKGROUND_COLOUR);
+
+            for (int i = 0; i < NUM_SONGS+1; i++) {
+            tft->drawString(SONG_DESCRIPTIONS[i], MENU_X_DATUM, MENU_Y_DATUM+23*i);
+            }
+
+            tft->setTextColor(HIGH_EMPHASIS_COLOUR, BACKGROUND_COLOUR);
+            tft->drawString(SONG_DESCRIPTIONS[currChoice], MENU_X_DATUM, MENU_Y_DATUM+23*currChoice);
+        }
+
+        tft->setTextColor(PRIMARY_TEXT_COLOUR, BACKGROUND_COLOUR);
+        prevChoice = currChoice;
+
+        prevLeft = currLeft;
+        prevRight = currRight;
+    }
+    tft->fillScreen(BACKGROUND_COLOUR);
+    chosenSong = currChoice;
+}
+
+void playSong(Song_t song, int barsToDisplay, TFT_eSPI *tft)
+{
+    int freq, n, minN, maxN, T;
+    for (n = 1 ; n <= NUM_FREQS ; n++) {
+        if (song.minFreq == TONE_INDEX[n]) minN = n;
+        if (song.maxFreq == TONE_INDEX[n]) {
+            maxN = n;
+            break;
+        }
+    }
+    const int songLength = song.numNotes;
+    const int dx = 320/barsToDisplay/song.bar;
+    const int dy = 150/(maxN - minN);
+    const int T0 = song.period;
+    const char *noteName;
+    int periods = 0;
+    unsigned long startTime;
+    int act_delay;
+    tft->setCursor(0, 0);
+    tft->printf("000/%-3d: --- %.13s", songLength, song.name);
+    tft->drawFastHLine(0, 20, 320, TFT_WHITE);
+    for (int i = 0, k = 0; i < songLength; i++) {
+        startTime = millis();
+        freq = song.notes[i].pitch;
+        noteName = song.notes[i].noteName;
+        T = song.notes[i].noteLength * T0;
+        tft->setCursor(0, 0);
+        if (periods % (barsToDisplay*song.bar) == 0) {
+            tft->fillRect(0, 21, 320, 149, TFT_BLACK); 
+            periods = 0;
+            k = !k;
+        }
+        if (freq) {
+            ledcWriteTone(TREBLE, freq);
+            //ledcWriteTone(BASS, 294);
+            for (n = minN ; n <= maxN ; n++) if (freq == TONE_INDEX[n]) break;
+            tft->drawFastHLine(periods*dx, 169-dy*(n-minN), dx*(T/T0)-2, HIGH_EMPHASIS_COLOUR);
+            if (song.overflow)
+                tft->printf("%3d/%-3d: %-3s %.13s", i+1, songLength, noteName, (k) ? song.name : song.overflow);
+            else
+                tft->printf("%3d/%-3d: %-3s %.13s", i+1, songLength, noteName, song.name);
+        } else {
+            ledcWriteTone(TREBLE, 0);
+            //ledcWriteTone(BASS, 294);
+            tft->drawFastHLine(periods*dx, 169, dx*(T/T0)-2, LOW_EMPHASIS_COLOUR);
+            tft->printf("%3d/%-3d: ", i+1, songLength);
+        }
+        periods += song.notes[i].noteLength;
+        act_delay = T - (millis() - startTime);
+        if (act_delay > 0) delay(act_delay);
+    }
+    ledcWriteTone(TREBLE, 0);
+    //ledcWriteTone(BASS, 0);
+}
---
+++ src/pitches.h
@@ -0,0 +1,190 @@
+#ifndef PITCHES_H
+#define PITCHES_H
+
+#define NUM_FREQS 90 // Range from B0 to DS8
+
+#define REST 0
+#define NOTE_B0  31
+#define C1  33
+#define CS1 35
+#define D1  37
+#define DS1 39
+#define E1  41
+#define F1  44
+#define FS1 46
+#define G1  49
+#define GS1 52
+#define A1  55
+#define AS1 58
+#define NOTE_B1  62
+#define C2  65
+#define CS2 69
+#define D2  73
+#define DS2 78
+#define E2  82
+#define F2  87
+#define FS2 93
+#define G2  98
+#define GS2 104
+#define A2  110
+#define AS2 117
+#define B2  123
+#define C3  131
+#define CS3 139
+#define D3  147
+#define DS3 156
+#define E3  165
+#define F3  175
+#define FS3 185
+#define G3  196
+#define GS3 208
+#define A3  220
+#define AS3 233
+#define B3  247
+#define C4  262
+#define CS4 277
+#define D4  294
+#define DS4 311
+#define E4  330
+#define F4  349
+#define FS4 370
+#define G4  392
+#define GS4 415
+#define A4  440
+#define AS4 466
+#define B4  494
+#define C5  523
+#define CS5 554
+#define D5  587
+#define DS5 622
+#define E5  659
+#define F5  698
+#define FS5 740
+#define G5  784
+#define GS5 831
+#define A5  880
+#define AS5 932
+#define B5  988
+#define C6  1047
+#define CS6 1109
+#define D6  1175
+#define DS6 1245
+#define E6  1319
+#define F6  1397
+#define FS6 1480
+#define G6  1568
+#define GS6 1661
+#define A6  1760
+#define AS6 1865
+#define B6  1976
+#define C7  2093
+#define CS7 2217
+#define D7  2349
+#define DS7 2489
+#define E7  2637
+#define F7  2794
+#define FS7 2960
+#define G7  3136
+#define GS7 3322
+#define A7  3520
+#define AS7 3729
+#define B7  3951
+#define C8  4186
+#define CS8 4435
+#define D8  4699
+#define DS8 4978
+
+const int TONE_INDEX[NUM_FREQS] = {
+    0,
+    31,
+    33,
+    35,
+    37,
+    39,
+    41,
+    44,
+    46,
+    49,
+    52,
+    55,
+    58,
+    62,
+    65,
+    69,
+    73,
+    78,
+    82,
+    87,
+    93,
+    98,
+    104,
+    110,
+    117,
+    123,
+    131,
+    139,
+    147,
+    156,
+    165,
+    175,
+    185,
+    196,
+    208,
+    220,
+    233,
+    247,
+    262,
+    277,
+    294,
+    311,
+    330,
+    349,
+    370,
+    392,
+    415,
+    440,
+    466,
+    494,
+    523,
+    554,
+    587,
+    622,
+    659,
+    698,
+    740,
+    784,
+    831,
+    880,
+    932,
+    988,
+    1047,
+    1109,
+    1175,
+    1245,
+    1319,
+    1397,
+    1480,
+    1568,
+    1661,
+    1760,
+    1865,
+    1976,
+    2093,
+    2217,
+    2349,
+    2489,
+    2637,
+    2794,
+    2960,
+    3136,
+    3322,
+    3520,
+    3729,
+    3951,
+    4186,
+    4435,
+    4699,
+    4978,
+};
+
+#endif
